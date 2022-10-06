1a7532503bdaaeab89e63da76fb55c98b61ffbdd
dikalee7 <51206302+dikalee7@users.noreply.github.com>
2022-10-06 16:40:38 +0900

    commit

---
+++ point/src/main/java/wallet/domain/PointsDeducted.java
@@ -0,0 +1,22 @@
+package wallet.domain;
+
+import wallet.domain.*;
+import wallet.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class PointsDeducted extends AbstractEvent {
+
+    private Long id;
+    private Long amount;
+    private String holder;
+
+    public PointsDeducted(Point aggregate){
+        super(aggregate);
+    }
+    public PointsDeducted(){
+        super();
+    }
+}
